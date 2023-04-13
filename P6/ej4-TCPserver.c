bd24664aea945442d658a5599759a95203edfa3c
andreu <andreu@debian-BULLSEYE-live-builder-AMD64>
2023-04-13 23:41:49 +0200

    P6 acabada

--- P6/ej4-TCPserver.c
+++ P6/ej4-TCPserver.c
@@ -8,39 +8,45 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
-#define server_ip "127.0.0.1"
-#define server_port 9002
+#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT 9002
 
 int main(int argc, char const *argv[])
 {
     int newfd;
-    char server_message[128] = "Te has conectado correctamente al servidor!\n";
+    char server_message[128];
 
-
-    //Asignamos al servidor la IP del ordenador que corre el programa
+    //Creamos socket para este proceso (servidor)
     int server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
     printf("server_sockfd = %d\n", server_sockfd);
-    
 
     //Escogemos una dirección IP al socket del servidor
     struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET; 
-    server_addr.sin_port = htons(9002);
-    server_addr.sin_addr.s_addr = inet_addr(server_ip);
+    server_addr.sin_port = htons(SERVER_PORT);
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
 
-
-    //Asignamos el socket que hemos creado al puerto de la máquina (le decimos al OS que asigne esa addr y puerto a nuestro socket)
+    //Asignamos el socket que hemos creado al address y el puerto de la máquina que hemos especificado en server_addr
     int bind_ret = bind(server_sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
-    printf("bind_ret = %d\n", bind_ret);
+    printf("bind_ret = %d\n\n", bind_ret);
     if (bind_ret < 0) return EXIT_FAILURE;
 
-
-    //Escuchamos peticiones de conexión, las aceptamos todas y escribimos un mensaje
+    //Escuchamos peticiones de conexión y las aceptamos todas (nos da igual el address de donde vengan por eso los campos son NULL)
     listen(server_sockfd, 5);
     int client_sockfd = accept(server_sockfd, NULL, NULL);
-    //int send_ret = send(client_socketfd, &server_message, sizeof(server_message), 0);
-    int write_ret = write(client_sockfd, &server_message, sizeof(server_message));
-    printf("write_ret = %d\n", write_ret);
+
+    //Enviamos los números del 1 al 10 y un mensaje;
+    int write_ret;
+    for (int i = 0; i < 10; i++)
+    {
+        sprintf(server_message, "Número enviado = %d\n", i);
+        //write_ret = send(client_sockfd, &server_message, sizeof(server_message), 0);
+        write_ret = write(client_sockfd, &server_message, sizeof(server_message));
+        printf("%swrite_ret = %d\n", server_message, write_ret);
+    }
+    sprintf(server_message, "Ya hemos enviado todos los números :D\n");
+    write_ret = write(client_sockfd, &server_message, sizeof(server_message));
+    printf("%swrite_ret = %d\n", server_message, write_ret);
 
     //Cerramos socket
     close(server_sockfd);
--- P6/ej4-client.c
+++ P6/ej4-client.c
@@ -8,30 +8,34 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
-#define server_ip "127.0.0.1"
-#define server_port 9002
+#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT 9002
 
 int main(int argc, char const *argv[])
 {
-    //Creamos un socket que será el cliente
+    //Creamos socket para este proceso (cliente)
     int client_sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-    //Creamos el objeto la address del servidor para poder conectarnos a el
+    //Creamos el objeto de la address del servidor para poder conectarnos a el
     struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET; 
-    server_addr.sin_port = htons(9002);
-    server_addr.sin_addr.s_addr = inet_addr(server_ip);
+    server_addr.sin_port = htons(SERVER_PORT);
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
 
     //Conectamos con el servidor
     int connect_ret = connect(client_sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
     printf("connect_ret = %d\n", connect_ret);
 
-    //Leemos un mensaje
+    //Leemos todos los mensajes y los mostramos en consola
     char buff[128];
-    int read_ret = read(client_sockfd, buff, 45);
-    //int read_ret = recv(client_sockfd, &buff, sizeof(buff), 0);
-    printf("read_ret = %d\n", read_ret);
-    printf(buff);
+    int read_ret = 1;
+    while (read_ret != 0 && read_ret != -1)
+    {
+        //read_ret = recv(client_sockfd, &buff, sizeof(buff), 0);
+        read_ret = read(client_sockfd, buff, sizeof(buff));
+        printf("read_ret = %d\n", read_ret);
+        printf(buff);
+    }
 
     //Cerramos socket
     close(client_sockfd);
