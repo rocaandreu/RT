4668f455d4c3d90cad0107b111ba0b2481d0a781
Pabloopp <126245759+Pabloopp@users.noreply.github.com>
2023-04-14 13:07:01 +0200

    Update LogServer.c

--- Summary/LogServer.c
+++ Summary/LogServer.c
@@ -8,12 +8,23 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <fcntl.h>
+#include <pthread.h>
+#include "Log.h"
 
+#define SERVER_IP "127.0.0.1"
 
+void *handler(void *i){
+    
+    
+    
+    //pthreadexit
+}
 int main(int argc, char const *argv[])
 {
     int logfd;
     char *logfile_name;
+    int port = atoi(argv[2]);
+    pthread_t th;
     strcpy(logfile_name, argv[1]);
     if (access(logfile_name, F_OK) == 0)    
     {
@@ -22,12 +33,36 @@ int main(int argc, char const *argv[])
     }
     else
     {
-        logfd = open(logfile_name, O_RDWR);
+        logfd = open(logfile_name, O_WRONLY|O_CREAT);
         if (logfd < 0) perror("Error creating file\n");
     }
 
+    //Creamos el socket
+    int server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
+    //Creamos un adress para asignar posteriormente
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET; 
+    server_addr.sin_port = htons(port);
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+
+    //Asignamos el adress al socket
+    int bind_ret = bind(server_sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    if (bind_ret < 0) return EXIT_FAILURE;
+
+    //esperamos a que se establezca conexión
+    listen(server_sockfd, 200); //preguntar
+    //se establece la conexión, al poner NULL en el segundo parámetro, no especificamos un adress específico
+    //al que conectarnos, nos conectaremos a todos los que soliciten conexión
+    while (1)
+    {
+        int client_sockfd = accept(server_sockfd, NULL, NULL);
+        pthread_create(&th, NULL, handler, NULL);//Aun no se lo que tengo que pasarle al handler, lo dejo en NULL pero OJO
+
+    }
     
+
+
     close(logfd);
     return 0;
 
